7d15ce9297af33704462b481af1489ac50380f53
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-10-20 15:47:17 +0700

    add electricity page

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   function handleFormStep() {
-    $("#formStep").steps();
+    if ($("#formStep").lenght != undefined) {
+      $("#formStep").steps();
+    }
   }
 
   function handleGuideStep() {
-    $("#guideStep").steps({
-      transitionEffect: "slideLeft",
-    });
+    if ($("#guideStep").lenght != undefined) {
+      $("#guideStep").steps({
+        transitionEffect: "slideLeft",
+      });
+    }
   }
 
   function handleTogglePassword() {
@@ -88,157 +92,274 @@ $(document).ready(function () {
     $("#humidityHighLow").toggleClass("show");
   });
 
-  //Select2
-  $(".js-select").select2({
-    placeholder: "Eg. dedekysf93@gmail.com",
-    allowClear: true,
-    width: "100%",
-  });
+  if ($(".js-select").length != undefined && $(".js-select").length > 0) {
+    $(".js-select").select2({
+      placeholder: "Eg. dedekysf93@gmail.com",
+      allowClear: true,
+      width: "100%",
+    });
+  }
 });
 
 // temp chart
-var ctx = document.getElementById("tempChart");
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00"],
-    datasets: [
-      {
-        label: "Living Room",
-        data: [22, 27, 33, 22, 25, 25],
-        backgroundColor: "rgb(235, 243, 248)",
-        borderColor: "rgb(235, 243, 248)",
-        fill: false,
+let ctxTemp = document.getElementById("tempChart");
+if (ctxTemp != null) {
+  const tempChart = new Chart(ctxTemp, {
+    type: "line",
+    data: {
+      labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00"],
+      datasets: [
+        {
+          label: "Living Room",
+          data: [22, 27, 33, 22, 25, 25],
+          backgroundColor: "rgb(235, 243, 248)",
+          borderColor: "rgb(235, 243, 248)",
+          fill: false,
+        },
+        {
+          label: "Ricky Bedroom",
+          data: [23, 23, 30, 25, 23, 23],
+          backgroundColor: "rgb(255, 193, 7)",
+          borderColor: "rgb(255, 193, 7)",
+          fill: false,
+        },
+        {
+          label: "Parent Bedroom",
+          data: [26, 26, 28, 20, 27, 27],
+          backgroundColor: "rgb(0, 123, 255)",
+          borderColor: "rgb(0, 123, 255)",
+          fill: false,
+        },
+        {
+          label: "Hot",
+          data: [60, 60, 60, 60, 60, 60], // max area hot
+          backgroundColor: "rgba(205, 98, 118, 0)",
+          borderColor: "rgba(205, 98, 118, 0)",
+          fill: { above: "rgba(205, 98, 118, 0.75)", target: { value: 29 } }, // min area hot
+        },
+        {
+          label: "Comfortable",
+          data: [29, 29, 29, 29, 29, 29],
+          backgroundColor: "rgba(77, 157, 46, 0)",
+          borderColor: "rgba(77, 157, 46, 0)",
+          fill: { above: "rgba(77, 157, 46, 0.75)", target: { value: 21 } },
+        },
+        {
+          label: "Cold",
+          data: [21, 21, 21, 21, 21, 21],
+          backgroundColor: "rgba(89, 162, 201, 0)",
+          borderColor: "rgba(89, 162, 201, 0)",
+          fill: { above: "rgba(89, 162, 201, 0.75)", target: { value: 0 } },
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            color: "#ccc",
+          },
+        },
       },
-      {
-        label: "Ricky Bedroom",
-        data: [23, 23, 30, 25, 23, 23],
-        backgroundColor: "rgb(255, 193, 7)",
-        borderColor: "rgb(255, 193, 7)",
-        fill: false,
-      },
-      {
-        label: "Parent Bedroom",
-        data: [26, 26, 28, 20, 27, 27],
-        backgroundColor: "rgb(0, 123, 255)",
-        borderColor: "rgb(0, 123, 255)",
-        fill: false,
-      },
-      {
-        label: "Hot",
-        data: [60, 60, 60, 60, 60, 60], // max area hot
-        backgroundColor: "rgba(205, 98, 118, 0)",
-        borderColor: "rgba(205, 98, 118, 0)",
-        fill: { above: "rgba(205, 98, 118, 0.75)", target: { value: 29 } }, // min area hot
-      },
-      {
-        label: "Comfortable",
-        data: [29, 29, 29, 29, 29, 29],
-        backgroundColor: "rgba(77, 157, 46, 0)",
-        borderColor: "rgba(77, 157, 46, 0)",
-        fill: { above: "rgba(77, 157, 46, 0.75)", target: { value: 21 } },
-      },
-      {
-        label: "Cold",
-        data: [21, 21, 21, 21, 21, 21],
-        backgroundColor: "rgba(89, 162, 201, 0)",
-        borderColor: "rgba(89, 162, 201, 0)",
-        fill: { above: "rgba(89, 162, 201, 0.75)", target: { value: 0 } },
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-        grid: {
-          color: "#ccc",
+
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
+        tooltip: {
+          backgroundColor: "rgba(255,255,255, 1)",
+          bodyColor: "#101B37",
+          titleColor: "#101B37",
         },
       },
     },
-
-    plugins: {
-      legend: {
-        position: "bottom",
-        display: false,
-      },
-      tooltip: {
-        backgroundColor: "rgba(255,255,255, 1)",
-        bodyColor: "#101B37",
-        titleColor: "#101B37",
-      },
-    },
-  },
-});
+  });
+}
 
 // humidity chart
-var ctx = document.getElementById("humidityChart");
-var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00"],
-    datasets: [
-      {
-        label: "Parent Bedroom",
-        data: [58, 62, 59, 62, 55, 59],
-        backgroundColor: "#007BFF",
-        borderColor: "#007BFF",
-        fill: false,
-      },
-      {
-        label: "Ricky Bedroom",
-        data: [55, 53, 52, 58, 53, 53],
-        backgroundColor: "#FFC107",
-        borderColor: "#FFC107",
-        fill: false,
-      },
-      {
-        label: "Living Room",
-        data: [53, 58, 54, 53, 59, 55],
-        backgroundColor: "#FFF",
-        borderColor: "#FFF",
-        fill: false,
-      },
-      {
-        label: "Damp",
-        data: [100, 100, 100, 100, 100, 100],
-        backgroundColor: "rgba(89,162,201, 0)",
-        borderColor: "rgba(89,162,201, 0)",
-        fill: { above: "rgba(89,162,201, 0.75)", target: { value: 60 } },
-      },
-      {
-        label: "Comfortable",
-        data: [60, 60, 60, 60, 60, 60],
-        backgroundColor: "rgba(77, 157, 46, 0)",
-        borderColor: "rgba(77, 157, 46, 0)",
-        fill: { above: "rgba(77, 157, 46, 0.75)", target: { value: 30 } },
-      },
-      {
-        label: "Dry",
-        data: [30, 30, 30, 30, 30, 30],
-        backgroundColor: "rgba(205,98,118, 0)",
-        borderColor: "rgba(205,98,118, 0)",
-        fill: { above: "rgba(205,98,118, 0.75)", target: { value: 0 } },
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+let ctxHumidity = document.getElementById("humidityChart");
+if (ctxHumidity != null) {
+  const humidityChart = new Chart(ctxHumidity, {
+    type: "line",
+    data: {
+      labels: ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00"],
+      datasets: [
+        {
+          label: "Parent Bedroom",
+          data: [58, 62, 59, 62, 55, 59],
+          backgroundColor: "#007BFF",
+          borderColor: "#007BFF",
+          fill: false,
+        },
+        {
+          label: "Ricky Bedroom",
+          data: [55, 53, 52, 58, 53, 53],
+          backgroundColor: "#FFC107",
+          borderColor: "#FFC107",
+          fill: false,
+        },
+        {
+          label: "Living Room",
+          data: [53, 58, 54, 53, 59, 55],
+          backgroundColor: "#FFF",
+          borderColor: "#FFF",
+          fill: false,
+        },
+        {
+          label: "Damp",
+          data: [100, 100, 100, 100, 100, 100],
+          backgroundColor: "rgba(89,162,201, 0)",
+          borderColor: "rgba(89,162,201, 0)",
+          fill: { above: "rgba(89,162,201, 0.75)", target: { value: 60 } },
+        },
+        {
+          label: "Comfortable",
+          data: [60, 60, 60, 60, 60, 60],
+          backgroundColor: "rgba(77, 157, 46, 0)",
+          borderColor: "rgba(77, 157, 46, 0)",
+          fill: { above: "rgba(77, 157, 46, 0.75)", target: { value: 30 } },
+        },
+        {
+          label: "Dry",
+          data: [30, 30, 30, 30, 30, 30],
+          backgroundColor: "rgba(205,98,118, 0)",
+          borderColor: "rgba(205,98,118, 0)",
+          fill: { above: "rgba(205,98,118, 0.75)", target: { value: 0 } },
+        },
+      ],
     },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
 
-    plugins: {
-      legend: {
-        position: "bottom",
-        display: false,
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
       },
     },
+  });
+}
+
+//Electricity Chart
+let ctxElectricity = document.getElementById("electricChart");
+if (ctxElectricity != null) {
+  const electricityChart = new Chart(ctxElectricity, {
+    type: "bar",
+    data: {
+      labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      datasets: [
+        {
+          label: "Electricity used",
+          data: [0, 42, 0, 0, 0, 0, 0],
+          backgroundColor: "#D62F0E",
+          borderColor: "#D62F0E",
+          fill: true,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
+      },
+    },
+  });
+}
+
+//Bill Chart
+let ctxBill = document.getElementById("billChart");
+if (ctxBill != null) {
+  const billChart = new Chart(ctxBill, {
+    type: "bar",
+    data: {
+      labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      datasets: [
+        {
+          label: "electricity cost",
+          data: [0, 32, 0, 0, 0, 0, 0],
+          backgroundColor: "#D62F0E",
+          borderColor: "#D62F0E",
+          fill: true,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
+      },
+    },
+  });
+}
+
+//Date Filter Handler
+function dateFilterHandler() {
+  let dateFilter = document.getElementsByName("date-filter")[0].value;
+  //change input type element with class date-filter
+  document.getElementsByClassName("date-filter")[0].type = dateFilter;
+}
+
+// //Carousel List
+// let items = document.querySelectorAll(".carousel .carousel-item");
+// console.log("items: ", items);
+
+// items.forEach((el) => {
+//   const minPerSlide = 3;
+//   let next = el.nextElementSibling;
+//   console.log("next: ", next);
+//   for (var i = 0; i < minPerSlide; i++) {
+//     if (!next) {
+//       // wrap carousel by using first child
+//       next = items[0];
+//     }
+//     let cloneChild = next.cloneNode(true);
+//     el.appendChild(cloneChild.children[0]);
+//     next = next.nextElementSibling;
+//   }
+// });
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 12,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-// datepicker
-$(".datepicker").datepicker({
-  uiLibrary: "bootstrap4",
-});
+// // datepicker
+// $(".datepicker").datepicker({
+//   uiLibrary: "bootstrap4",
+// });
+
+//litePicker
+// const picker = new Litepicker({
+//   element: document.getElementById("litepicker"),
+// });
