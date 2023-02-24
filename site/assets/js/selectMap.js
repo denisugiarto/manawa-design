a4a682eb276a384bbf58e3545cbe3cd91c9ec1e9
Deni Sugiarto <sugiarto.deni@gmail.com>
2023-02-24 14:19:47 +0700

    update ui step form

--- site/assets/js/selectMap.js
+++ site/assets/js/selectMap.js
@@ -5,28 +5,65 @@ let tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 });
 
-const map = L.map('selectMap', {
+var mapPreview = L.map('mapPreview', {
   center: PositionInit,
-  zoomControl: false,
   zoom: 13,
   layers: [tiles],
+  zoomControl: false,
 });
 
-var iconOptions = {
-  iconUrl: './assets/img/marker-select.svg',
-  iconSize: [51, 68],
-};
+setTimeout(function () {
+  mapPreview.invalidateSize();
+}, 250);
 
-var markerIcon = L.icon(iconOptions);
-var markerOptions = {
-  icon: markerIcon,
-  draggable: true,
-  autoPan: true,
-};
+$(document).ready(function () {
+  //Revalidate Map Size when modal show
+  $('#modalSelectMap').on('show.bs.modal', function () {
+    mapPreview.remove();
+    const selectMap = L.map('selectMap', {
+      center: PositionInit,
+      zoomControl: false,
+      zoom: 13,
+      layers: [tiles],
+    });
 
-map.on('click', function (e) {
-  const mc = new L.Marker([e.latlng.lat, e.latlng.lng], markerOptions).addTo(
-    map
-  );
+    var iconOptions = {
+      iconUrl: './assets/img/marker-select.svg',
+      iconSize: [51, 68],
+    };
+
+    var markerIcon = L.icon(iconOptions);
+    var markerOptions = {
+      icon: markerIcon,
+    };
+
+    selectMap.on('click', function (e) {
+      var myPositionMarker = new L.Marker(
+        [e.latlng.lat, e.latlng.lng],
+        markerOptions
+      ).addTo(selectMap);
+      const latitudePosition = e.latlng.lat;
+      const longitudePosition = e.latlng.lng;
+      console.log(`Latitude Position ${latitudePosition}`);
+      console.log(`Longitude Position ${longitudePosition}`);
+    });
+    setTimeout(function () {
+      selectMap.invalidateSize();
+    }, 250);
+  });
+
+  $('#buttonConfirm').click(function () {
+    selectMap.remove();
+
+    setTimeout(function () {
+      var mapPreview = L.map('mapPreview', {
+        center: PositionInit,
+        zoom: 13,
+        layers: [tiles],
+      });
+      setTimeout(function () {
+        mapPreview.invalidateSize();
+      }, 1000);
+    }, 1000);
+  });
 });
-$(document).ready(function () {});
